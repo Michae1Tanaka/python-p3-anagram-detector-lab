82cb8ce9505c76f48d7ab9aeb868883e147d38c2
Michael Tanaka <tanakam4214@gmail.com>
2023-07-16 15:21:35 -0700

    Lab Completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, list_of_words):
+        matches = []
+        for word in list_of_words:
+            word.lower()
+            if sorted(word) == sorted(self.word) and self.word != word:
+                matches.append(word)
+        return matches
